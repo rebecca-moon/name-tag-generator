2145f14cbe0cda2acd09e222224649f6ca2d3f5f
rebecca-moon <rebecca.n.moon@gmail.com>
2022-11-10 09:35:19 -0700

    Initialize version control

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import NameTagList from "./NameTagList.js";
+import UserInput from "./UserInput.js";
+
+class App extends Component {
+  state = {
+    names: []
+  };
+
+  addName = (name) => {
+    const newNames = [name, ...this.state.names];
+    this.setState({ names: newNames });
+  };
+
+  removeName = (clickedIndex) => {
+    const filterCallback = (_, index) => index !== clickedIndex;
+    const newNames = this.state.names.filter(filterCallback);
+    this.setState({ names: newNames });
+  };
+
+  componentDidUpdate() {
+    const savedNameString = JSON.stringify(this.state.names);
+    localStorage.setItem("savedNames", savedNameString);
+  }
+
+  componentDidMount() {
+    const savedNameString = localStorage.getItem("savedNames");
+    if (savedNameString) {
+      const savedNames = JSON.parse(savedNameString);
+      this.setState({ names: savedNames });
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Name Tag Generator</h1>
+        <UserInput addName={this.addName} />
+        <NameTagList names={this.state.names} removeName={this.removeName} />
+      </div>
+    );
+  }
+}
+
+export default App;
